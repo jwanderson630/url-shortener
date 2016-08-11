524994186bff1ed0f76c4e77fbbe411831eb6928
Joe Anderson <yoej262@gmail.com>
2016-08-11 05:52:57 +0000

    Working Version

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 var express = require('express');
-
 var app = express();
-var getIP = require('ipware')().get_ip;
-var useragent= require("useragent");
-app.get('/', function (req, res) {
-    var agent = useragent.parse(req.headers['user-agent']).os.toString();
-    var ipInfo = getIP(req).clientIp;
-    var language = req.headers["accept-language"].split(",")[0];
-    var returnData = {"ipaddress" : ipInfo, "language" : language, "software" : agent};
-    res.send(returnData);
+var shorturl = require('shorturl');
+var googl = require('goo.gl');
+googl.setKey('AIzaSyAmO8l8hUZqY192Wu-o-ghaoVpshcl8n8c');
+googl.getKey();
+app.get(/^\/(.+)/, function(req, res) {
+  var originalUrl = req.originalUrl.slice(1,-1);
+  var finalResponse = {"originalURL" : null, "shortenedURL" : null};
+    googl.shorten(originalUrl)
+    .then(function(result){
+      console.log(originalUrl);
+      console.log(result);
+      finalResponse.originalURL = originalUrl;
+      finalResponse.shortenedURL = result;
+      res.send(finalResponse);
+    }).catch(function(err){
+      console.log(err.message);
+      finalResponse = {"error":err.message};
+      res.send(finalResponse);
+    });
 });
-
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080');
+  console.log('Url Shortening app listening on port 8080');
 });
